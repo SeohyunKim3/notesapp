97832385e8db501818973bb75a4d3c72a6934ec6
Seohyun Kim <ksh030208@snu.ac.kr>
2024-09-18 12:23:28 +0900

    123

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -19,6 +19,9 @@ function Books() {
         <div className="row">
             <h1 className="heading">당신의 호기심을 자극하는 해석이 있나요?</h1>
             <PostMasonry posts={trending}/>
+        <div className="secondrow">
+            <h1 className="subheading">당신은 무엇을 궁금해하나요?</h1>
+        </div>
         </div>
     </section>
     )
