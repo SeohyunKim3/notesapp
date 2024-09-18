b3e4d5db5a7d5a9c3163deb80097afaaaad8d055
Seohyun Kim <ksh030208@snu.ac.kr>
2024-09-18 19:37:06 +0900

    123

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Login() {
-    return (
-        <div>Login</div>
-    )
-}
+    const PopupLogin = () => {
+        const [isOpen, setIsOpen] = useState(false);
+      
+        const openLoginPopup = () => setIsOpen(true);
+        const closeLoginPopup = () => setIsOpen(false);
+
+        PopupLogin()
+      
+        return (
+          <div>
+            <button onClick={openLoginPopup}>Login</button>
+            {isOpen && (
+              <div className="login-popup">
+                <button onClick={closeLoginPopup}>Close</button>
+                <form>
+                  <input type="text" placeholder="Username" />
+                  <input type="password" placeholder="Password" />
+                  <button type="submit">Submit</button>
+                </form>
+              </div>
+            )}
+          </div>
+        )
+}}
 
 export default Login;
